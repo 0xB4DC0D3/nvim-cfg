bdc756ca1bb909d3eb894716f117632dd5413622
Nameless <enigma@Bruh.local>
2024-09-17 15:38:53 +0300

    fix(lsp): now autocomplete has the full list of vars/properties from the language server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,10 +9,14 @@ return {
     }
   },
   config = function(_, opts) 
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
     require("mason").setup(opts)
     require("mason-lspconfig").setup_handlers({
       function (server_name)
-        require("lspconfig")[server_name].setup({})
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities
+        })
       end
     })
   end
